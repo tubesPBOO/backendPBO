89013028262341d91381479723a8f5d6ad432aef
arsyadfillah <arsyadfillah@gmail.com>
2025-05-12 23:48:39 +0700

    Update function

--- src/main/java/com/example/tubespboo/model/Material.java
+++ src/main/java/com/example/tubespboo/model/Material.java
@@ -4,4 +4,17 @@ public class Material {
     private int id,stock;
     private String name;
     private double rating,price;
+    
+    public void updateStock(int stock){
+        this.stock = stock;
+    }
+    public void updatePrice(double price){
+        this.price = price;
+    }
+    public void addRating(double rating){
+        this.rating = rating;
+    }
+    public double getAverageRating(){
+        return 0;
+    }
 }
