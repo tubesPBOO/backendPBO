11401e7fcb47ebada663dc8211e50bf37481ffa9
arsyadfillah <arsyadfillah@gmail.com>
2025-05-12 22:20:47 +0700

    Menyelesaikan kerangka class material

--- src/main/java/com/example/tubespboo/services/AdminServices.java
+++ src/main/java/com/example/tubespboo/services/AdminServices.java
@@ -1,5 +1,26 @@
 package com.example.tubespboo.services;
 
-public class AdminServices {
-    
+public class AdminServices extends UserServices {
+    public void addMaterial(Material m){
+        
+    }
+    public void updateMaterialStock(Material m){
+
+    }
+    public void updatePrice(int id,double price){
+        
+    }
+    public void registerWorker(Tukang){
+
+    }
+    public void assignTukangToOrder(int orderId,int tukangId){
+
+    }
+    public void UpdateTukangAvailability(int tukangId,boolean available){
+
+    }
+    @Override
+    public void viewDashboard(){
+        
+    }
 }
