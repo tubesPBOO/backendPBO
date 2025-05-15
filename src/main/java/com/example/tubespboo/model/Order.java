a847aee943162396c4041ebe84c387e6fcc60d61
arsyadfillah <arsyadfillah@gmail.com>
2025-05-15 16:35:02 +0700

    Update order

---
+++ src/main/java/com/example/tubespboo/model/Order.java
@@ -0,0 +1,23 @@
+package com.example.tubespboo.model;
+
+import java.util.List;
+
+public class Order {
+    private int id;
+    private String name;
+    private double price;
+    private List<Material> materials;
+    private List<Tukang> tukangs;
+
+    public double calculateTotal() {
+
+    }
+
+    public void addMaterial() {
+
+    }
+
+    public void addTukang() {
+
+    }
+}
